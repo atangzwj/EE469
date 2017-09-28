89cd79a5c813781f6c888801513a3eeb4c4d32fa
radleigh <radleigh@msn.com>
2017-09-27 23:38:00 -0700

    fixed indentation regfile

--- lab1/regfile.sv
+++ lab1/regfile.sv
@@ -1,12 +1,14 @@
 module regfile (
-    output logic [63:0] ReadData1,
-    output logic [63:0] ReadData2,
-    input  logic [63:0] WriteData,
-    input  logic [4:0]  ReadRegister1,
-    input  logic [4:0]  ReadRegister2,
-    input  logic [4:0]  WriteRegister,
-    input               RegWrite,
-    input               clk,
-    );
+   output logic [63:0] ReadData1,
+   output logic [63:0] ReadData2,
+   input  logic [63:0] WriteData,
+   input  logic [4:0]  ReadRegister1,
+   input  logic [4:0]  ReadRegister2,
+   input  logic [4:0]  WriteRegister,
+   input               RegWrite,
+   input               clk,
+   );
 
+    
+    
 endmodule
