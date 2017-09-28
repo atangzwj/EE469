02239bd1ee21395f257d475cffdbbfc139241837
radleigh <radleigh@msn.com>
2017-09-27 22:33:29 -0700

    mux2_1 tested working

--- lab1/muxes/mux2_1.sv
+++ lab1/muxes/mux2_1.sv
@@ -1,6 +1,6 @@
 module mux2_1(
-   output logic out;
-   input  logic i0, il, sel;
+   output logic out,
+   input  logic i0, i1, sel
    );
 
    // Wires
@@ -13,5 +13,24 @@ module mux2_1(
    or(out, out0, out1);
 
 endmodule
+
+module mux2_1_testbench();
+   logic i0, i1, sel;
+   logic out;
+
+   mux2_1 dut (.out, .i0, .i1, .sel);
+
+   initial begin
+      sel=0; i0=0; i1=0; #10;
+      sel=0; i0=0; i1=1; #10;
+      sel=0; i0=1; i1=0; #10;
+      sel=0; i0=1; i1=1; #10;
+      sel=1; i0=0; i1=0; #10;
+      sel=1; i0=0; i1=1; #10;
+      sel=1; i0=1; i1=0; #10;
+      sel=1; i0=1; i1=1; #10;
+   end
+endmodule
+
 
 
