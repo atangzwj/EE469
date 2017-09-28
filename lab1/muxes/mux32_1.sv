a62b583019aa0113c873fce1a5a9af3f3a080067
radleigh <radleigh@msn.com>
2017-09-27 19:28:05 -0700

    fixed some stuff for muxes

--- lab1/muxes/mux32_1.sv
+++ lab1/muxes/mux32_1.sv
@@ -1,15 +1,17 @@
 module mux32_1(
    output logic       out;
-   input  logic [2:0] sel;
-   input  logic [7:0] in;
+   input  logic [4:0] sel;
+   input  logic [31:0] in;
    );
 
    // Wires
    logic [1:0] mOut;
 
-   mux8_1 m0(.out(mOut[0]), .i0(in[0]), .i1(in[1]), .sel(sel[1:0]); 
-   mux8_1 m1(.out(mOut[1]), .i0(in[2]), .i1(in[3]), .sel(sel[1:0]); 
-   mux4_1 m2(.out(out), .i0(mOut[0]), .i1(mOut[1]), .sel(sel[2])); 
+   mux8_1 m0(.out(mOut[0]), .i0(in[0]), .i1(in[1]), .sel(sel[2:0])); 
+   mux8_1 m1(.out(mOut[1]), .i0(in[2]), .i1(in[3]), .sel(sel[2:0])); 
+   mux8_1 m2(.out(mOut[1]), .i0(in[2]), .i1(in[3]), .sel(sel[2:0])); 
+   mux8_1 m3(.out(mOut[1]), .i0(in[2]), .i1(in[3]), .sel(sel[2:0])); 
+   mux4_1 m4(.out(out), .i0(mOut[0]), .i1(mOut[1]), .sel(sel[4:3])); 
 
 endmodule
 
--- lab1/muxes/mux8_1.sv
+++ lab1/muxes/mux8_1.sv
@@ -7,8 +7,8 @@ module mux8_1(
    // Wires
    logic [1:0] mOut;
 
-   mux4_1 m0(.out(mOut[0]), .i0(in[0]), .i1(in[1]), .sel(sel[1:0]); 
-   mux4_1 m1(.out(mOut[1]), .i0(in[2]), .i1(in[3]), .sel(sel[1:0]); 
+   mux4_1 m0(.out(mOut[0]), .i0(in[0]), .i1(in[1]), .sel(sel[1:0])); 
+   mux4_1 m1(.out(mOut[1]), .i0(in[2]), .i1(in[3]), .sel(sel[1:0])); 
    mux2_1 m2(.out(out), .i0(mOut[0]), .i1(mOut[1]), .sel(sel[2])); 
 
 endmodule
