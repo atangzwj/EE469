923d6435cd92cc9e622ccf7f5b72b5582c3cef89
Radleigh Ang <radleigh@uw.edu>
2017-10-10 13:10:11 -0700

    added 64bit and or and xor, and testbench to test these logics

--- alu/alu.sv
+++ alu/alu.sv
@@ -23,5 +23,52 @@ module alu (
    // 100:			result = bitwise A & B		value of overflow and carry_out unimportant
    // 101:			result = bitwise A | B		value of overflow and carry_out unimportant
    // 110:			result = bitwise A XOR B	value of overflow and carry_out unimportant
-
+   
+   logic [63:0] andOut, orrOut, xorOut; 
+   genvar i;
+   generate
+      for (i = 0; i < 64; i++) begin : bitwiseGates
+         and #50 a1 (andOut[i], A[i], B[i]);
+         or  #50 o1 (orrOut[i], A[i], B[i]);
+         xor #50 x1 (xorOut[i], A[i], B[i]);
+      end
+   endgenerate
+    
 endmodule
+
+module alu_testbench ();
+   logic [63:0] result;
+   logic        negative,
+                zero,
+                overflow,
+                carry_out;
+   logic [63:0] A, B;
+   logic  [2:0] cntrl;
+   
+   alu dut(.result, .negative, .zero, .overflow, .carry_out, .A, .B, .cntrl);
+   initial begin
+      A = 64'h               0; // DEFAULT case
+      B = 64'h               0; #1000;
+
+      A = 64'hFFFFFFFFFFFFFFFF; // OR case 1
+      B = 64'h               0; #1000;
+      
+      A = 64'h               0; // OR case 2    
+      B = 64'hFFFFFFFFFFFFFFFF; #1000;
+
+      A = 64'h0101010101010101; // XOR case1
+      B = 64'h1010101010101010; #1000;
+      
+      A = 64'h1010101010101010; // XOR case2
+      B = 64'h0101010101010101; #1000;
+
+      A = 64'hFFFFFFFFFFFFFFFF; // AND case
+      B = 64'hFFFFFFFFFFFFFFFF; #1000;
+   
+   end
+endmodule
+
+
+
+
+
