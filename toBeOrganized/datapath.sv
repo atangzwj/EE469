82156f2550c38104a9e3056661e2c10a262ff60b
radleigh <radleigh@msn.com>
2017-10-18 00:00:02 -0700

    added skeleton instantiations for datamem and instructmem, the heck is the output for this module?

---
+++ toBeOrganized/datapath.sv
@@ -0,0 +1,35 @@
+module datapath (
+   output logic ????;
+   input  logic Reg2Loc,
+                ALUSrc,
+                MemToReg,
+                RegWrite,
+                MemWrite,
+                BrTaken,
+                UncondBr,
+                ALUOp
+                
+                );
+   
+   // control logic 
+
+   );
+   
+   // 5 2_1 muxes
+   
+   // 3 ALUs (2 only adders)
+
+   // Instruction Memory instantiation
+   // Input a 64bit adx and outputs 32bit instruction
+   instructmem name (.clk, .instruction(),
+                        .address());
+   
+   // Data Memory instantiation
+	datamem name (.clk, .read_data(),
+                  .address(),
+                  .write_enable(),
+                  .write_data(),
+                  .xfer_size());
+   
+   
+endmodule
