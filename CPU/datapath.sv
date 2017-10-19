76c3b61437b335455098cf0b980ebbddd4051b32
Radleigh Ang <radleigh@uw.edu>
2017-10-19 13:53:01 -0700

    added skeleton logic for regfile instantiation and add/subtract

--- CPU/datapath.sv
+++ CPU/datapath.sv
@@ -1,20 +1,25 @@
 `timescale 1ns/10ps
 
 module datapath (
-   input  logic clk, reset
+   input logic       clk, reset,
+   input logic [4:0] Rd, Rm, Rn,
+   input logic [2:0] ALUOp
    );
    
    logic [63:0] instAddr, instAddrNext;
    logic [31:0] instruction;
    
-   // Program Counter, 
+   // Program Counter
    reg64 pc (.clk,
              .reset,
              .dOut(instAddr),
              .WriteData(instAddrNext),
              .wrEnable(1'b1));
-   
+             
+   // Instruction Memory
    instructmem im (.clk, .instruction, .address(instAddr));
+   
+   // Next instruction address: Adds 4 to the instruction memory
    alu addInst_4 (.result(instAddrNext),
                   .negative(),
                   .zero(),
@@ -23,6 +28,29 @@ module datapath (
                   .A(instAddr),
                   .B(64'd4),
                   .cntrl(3'b010));
+   
+   // Regfile
+   logic [63:0] Dw, Da, Db;
+   regfile rf (.clk,
+               .ReadData1(Da),
+               .ReadData2(Db),
+               .WriteData(),
+               .ReadRegister1(),
+               .ReadRegister2(),
+               .WriteREgister(),
+               .RegWrite()
+               );
+   
+   alu gpr (.result(Dw),
+            .negative(),
+            .zero(),
+            .overflow(),
+            .carry_out(),
+            .A(Da),
+            .B(Db),
+            .cntrl(ALUOp));   
+   
+   
 endmodule
 
 module datapath_testbench ();
