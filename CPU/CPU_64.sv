5dc5597cdf7c796ff6da94aea9642ab6d6a4e42e
Radleigh Ang <radleigh@uw.edu>
2017-10-23 11:22:33 -0700

    added skeleton for CPU_64

---
+++ CPU/CPU_64.sv
@@ -0,0 +1,27 @@
+`timescale 1ns/10ps
+
+module CPU_64 (
+   input logic clk, reset,
+   );
+   
+endmodule
+
+module CPU_64_testbench ();
+   logic clk, reset;
+
+   CPU_64 dut (.clk, .reset);
+
+   parameter CLK_PERIOD = 10;
+   initial begin
+      clk <= 0;
+      forever #(CLK_PERIOD / 2) clk <= ~clk;
+   end
+   
+   integer i;   
+   initial begin
+   reset <= 1'b1; @(posedge clk);
+   reset <= 1'b0; @(posedge clk);
+   
+   $stop;
+   end
+endmodule
