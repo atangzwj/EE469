441f1b8c19b79f0bbb2a217a03d908e14be96e27
radleigh <radleigh@msn.com>
2017-10-26 11:14:09 -0700

    added selectData with I guess a not clean top bit parameter default arvin you duck

---
+++ CPU/selectData.sv
@@ -0,0 +1,18 @@
+// Used to select one of two buses 
+module selectData #(parameter TOP_BIT = 63) (
+   output logic [TOP_BIT:0] out, 
+   input  logic [TOP_BIT:0] A,
+   input  logic [TOP_BIT:0] B,
+   input  logic             sel
+   );
+
+   genvar i;
+   generate
+      for (i = 0; i < TOP_BIT + 1; i++) begin : muxing
+         mux2_1 m (.out(out[i]),
+                   .i0(A[i]),
+                   .i1(B[i]),
+                   .sel);
+      end
+   endgenerate
+endmodule
