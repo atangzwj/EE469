9339e4de022787948d9c7d3888ad7b0bd0901cbd
atangzwj <atangzwj@outlook.com>
2017-10-26 11:19:25 -0700

    Edited selectData module Parameter is now the size of data being selected Some code formatting

--- CPU/selectData.sv
+++ CPU/selectData.sv
@@ -1,18 +1,22 @@
+`timescale 1ns/10ps
+
 // Used to select one of two buses 
-module selectData #(parameter TOP_BIT = 63) (
-   output logic [TOP_BIT:0] out, 
-   input  logic [TOP_BIT:0] A,
-   input  logic [TOP_BIT:0] B,
-   input  logic             sel
+module selectData #(parameter TOP_BIT = 64) (
+   output logic [TOP_BIT - 1 : 0] out, 
+   input  logic [TOP_BIT - 1 : 0] A,
+   input  logic [TOP_BIT - 1 : 0] B,
+   input  logic                   sel
    );
 
    genvar i;
    generate
       for (i = 0; i < TOP_BIT + 1; i++) begin : muxing
-         mux2_1 m (.out(out[i]),
-                   .i0(A[i]),
-                   .i1(B[i]),
-                   .sel);
+         mux2_1 m (
+            .out(out[i]),
+            .i0(A[i]),
+            .i1(B[i]),
+            .sel
+         );
       end
    endgenerate
 endmodule
