c12fb6089b1706f5140408d1ed42928b927dc652
atangzwj <atangzwj@outlook.com>
2017-10-26 11:21:08 -0700

    Further edits to selectData Renamed parameter Edited code for new parameter

--- CPU/selectData.sv
+++ CPU/selectData.sv
@@ -1,16 +1,16 @@
 `timescale 1ns/10ps
 
 // Used to select one of two buses 
-module selectData #(parameter TOP_BIT = 64) (
-   output logic [TOP_BIT - 1 : 0] out, 
-   input  logic [TOP_BIT - 1 : 0] A,
-   input  logic [TOP_BIT - 1 : 0] B,
+module selectData #(parameter WIDTH = 64) (
+   output logic [WIDTH - 1 : 0] out, 
+   input  logic [WIDTH - 1 : 0] A,
+   input  logic [WIDTH - 1 : 0] B,
    input  logic                   sel
    );
 
    genvar i;
    generate
-      for (i = 0; i < TOP_BIT + 1; i++) begin : muxing
+      for (i = 0; i < WIDTH; i++) begin : muxing
          mux2_1 m (
             .out(out[i]),
             .i0(A[i]),
