9ae3d0e7deb2b456fcd91bb34a9721e8b8f63958
atangzwj <atangzwj@outlook.com>
2017-11-02 11:20:28 -0700

    Wrote control logic, parsed instruction for fields

--- CPU/CPU_64.sv
+++ CPU/CPU_64.sv
@@ -1,11 +1,36 @@
 `timescale 1ns/10ps
 
-module CPU_64 (
-   input logic clk, reset
-   );
+module CPU_64 (clk, reset);
+   input logic clk, reset;
 
    logic [63:0] instrAddr, instrAddrNext;
    logic [31:0] instruction;
+
+   // Parse instruction for fields
+   logic [10:0] opcode;
+   assign opcode = instruction[31:21];
+   
+   logic [18:0] condAddr19;
+   logic [25:0] brAddr26;
+   assign brAddr26   = instruction[25:0];
+   assign condAddr19 = instruction[23:5];
+
+   logic [4:0] Rd, Rm, Rn;
+   assign Rd = instruction[4:0];
+   assign Rm = instruction[20:16];
+   assign Rn = instruction[9:5];
+
+   logic [8:0] Daddr9;
+   assign Daddr9 = instruction[20:12];
+
+   logic [11:0] Imm12;
+   assign Imm12 = instruction[21:10];
+
+   logic [1:0] Shamt;
+   assign Shamt = instruction[22:21];
+
+   logic [15:0] Imm16;
+   assign Imm16 = instruction[20:5];
 
    // Program Counter
    reg64 pc (
@@ -16,14 +41,14 @@ module CPU_64 (
       .wrEnable(1'b1)
    );
 
-   logic [18:0] condAddr19;
-   logic [25:0] brAddr26;
+   // Sign extend condAddr19 and brAddr26
    logic [63:0] condAddr19_SE, brAddr26_SE;
    assign condAddr19    = instruction[23:5];
    assign brAddr26      = instruction[25:0];
    assign condAddr19_SE = {{45{condAddr19[18]}}, condAddr19};
    assign brAddr26_SE   = {{38{brAddr26[25]}}, brAddr26};
 
+   // Select between conditional or unconditional branch amount
    logic [63:0] brChoice, brChoice4x;
    selectData branchSelector (
       .out(brChoice),
@@ -74,24 +99,29 @@ module CPU_64 (
 
    // Instruction Memory
    instructmem iMem (.address(instrAddr), .instruction, .clk);
-endmodule
 
-module CPU_64_testbench ();
-   logic clk, reset;
-
-   CPU_64 dut (.clk, .reset);
-
-   parameter CLK_PERIOD = 10;
-   initial begin
-      clk <= 0;
-      forever #(CLK_PERIOD / 2) clk <= ~clk;
-   end
-
-   integer i;   
-   initial begin
-   reset <= 1'b1; @(posedge clk);
-   reset <= 1'b0; @(posedge clk);
-   
-   $stop;
-   end
+   // Datapath
+   datpath dp (
+      .clk,
+      .reset,
+      .flags,
+      .Rd,
+      .Rm,
+      .Rn,
+      .Daddr9,
+      .Imm12,
+      .Shamt,
+      .Imm16,
+      .Reg2Loc,
+      .ALUSrc,
+      .MemToReg,
+      .RegWrite,
+      .MemWrite,
+      .MemRead,
+      .ChooseImm,
+      .xferByte,
+      .ChooseMovk,
+      .ChooseMovz,
+      .ALUOp
+   );
 endmodule
--- CPU/main_control.sv
+++ CPU/main_control.sv
@@ -1,11 +1,234 @@
 `timescale 1ns/10ps
 
 module main_control (
-   output logic reg2Loc,
-   output logic ALU_src,
-   output logic memToReg,
-   input  logic [20:0] opcode;
+   output logic        Reg2Loc,
+   output logic        ALUSrc,
+   output logic        MemToReg,
+   output logic        RegWrite,
+   output logic        MemWrite,
+   output logic        MemRead,
+   output logic        ChooseImm,
+   output logic        xferByte,
+   output logic        BrTaken,
+   output logic        UncondBr,
+   output logic        ChooseMovk,
+   output logic        ChooseMovz,
+   output logic  [2:0] ALUOp,
+   input  logic [10:0] opcode,
+   input  logic  [3:0] flags
    );
 
-   
+   // Intstruction opcodes
+   parameter
+   B     = 10'b000_101x_xxxx,
+   CBZ   = 10'b101_1010_0xxx,
+   B_LT  = 10'b010_1010_0xxx,
+   ADDS  = 10'b101_0101_1000,
+   SUBS  = 10'b111_0101_1000,
+   ADDI  = 10'b100_1000_1000,
+   LDUR  = 10'b111_1100_0010,
+   LDURB = 10'b001_1100_0010,
+   STUR  = 10'b111_1100_0000,
+   STURB = 10'b001_1100_0000,
+   MOVK  = 10'b001_1110_0101,
+   MOVZ  = 10'b001_1010_0101;
+
+   always_comb begin
+      casex (opcode)
+         B:        begin
+                      Reg2Loc    = 1'bx;
+                      ALUSrc     = 1'bx;
+                      MemToReg   = 1'bx;
+                      RegWrite   = 1'b0;
+                      MemWrite   = 1'b0;
+                      MemRead    = 1'bx;
+                      ChooseImm  = 1'bx;
+                      xferByte   = 1'bx;
+                      BrTaken    = 1'b1;
+                      UncondBr   = 1'b1;
+                      ChooseMovk = 1'bx;
+                      ChooseMovz = 1'bx;
+                      ALUOp      = 3'bxxx;
+                   end
+         CBZ:      begin
+                      Reg2Loc    = 1'bx;
+                      ALUSrc     = 1'b0;
+                      MemToReg   = 1'bx;
+                      RegWrite   = 1'b0;
+                      MemWrite   = 1'b0;
+                      MemRead    = 1'bx;
+                      ChooseImm  = 1'bx;
+                      xferByte   = 1'bx;
+                      BrTaken    = flags[2];
+                      UncondBr   = 1'b0;
+                      ChooseMovk = 1'bx;
+                      ChooseMovz = 1'bx;
+                      ALUOp      = 3'b000;
+                   end
+         B_LT:     begin
+                      Reg2Loc    = 1'bx;
+                      ALUSrc     = 1'b0;
+                      MemToReg   = 1'bx;
+                      RegWrite   = 1'b0;
+                      MemWrite   = 1'b0;
+                      MemRead    = 1'bx;
+                      ChooseImm  = 1'bx;
+                      xferByte   = 1'bx;
+                      BrTaken    = flags[3] ^ flags[1];
+                      UncondBr   = 1'b0;
+                      ChooseMovk = 1'bx;
+                      ChooseMovz = 1'bx;
+                      ALUOp      = 3'bxxx;
+                   end
+         ADDS:     begin
+                      Reg2Loc    = 1'b1;
+                      ALUSrc     = 1'b0;
+                      MemToReg   = 1'b0;
+                      RegWrite   = 1'b1;
+                      MemWrite   = 1'b0;
+                      MemRead    = 1'bx;
+                      ChooseImm  = 1'b0;
+                      xferByte   = 1'bx;
+                      BrTaken    = 1'b0;
+                      UncondBr   = 1'bx;
+                      ChooseMovk = 1'b0;
+                      ChooseMovz = 1'b0;
+                      ALUOp      = 3'b010;
+                   end
+         SUBS:     begin
+                      Reg2Loc    = 1'b1;
+                      ALUSrc     = 1'b0;
+                      MemToReg   = 1'b0;
+                      RegWrite   = 1'b1;
+                      MemWrite   = 1'b0;
+                      MemRead    = 1'bx;
+                      ChooseImm  = 1'b0;
+                      xferByte   = 1'bx;
+                      BrTaken    = 1'b0;
+                      UncondBr   = 1'bx;
+                      ChooseMovk = 1'b0;
+                      ChooseMovz = 1'b0;
+                      ALUOp      = 3'b011;
+                   end
+         ADDI:     begin
+                      Reg2Loc    = 1'bx;
+                      ALUSrc     = 1'bx;
+                      MemToReg   = 1'b0;
+                      RegWrite   = 1'b1;
+                      MemWrite   = 1'b0;
+                      MemRead    = 1'bx;
+                      ChooseImm  = 1'b1;
+                      xferByte   = 1'bx;
+                      BrTaken    = 1'b0;
+                      UncondBr   = 1'bx;
+                      ChooseMovk = 1'b0;
+                      ChooseMovz = 1'b0;
+                      ALUOp      = 3'b010;
+                   end
+         LDUR:     begin
+                      Reg2Loc    = 1'bx;
+                      ALUSrc     = 1'b1;
+                      MemToReg   = 1'b1;
+                      RegWrite   = 1'b1;
+                      MemWrite   = 1'b0;
+                      MemRead    = 1'b1;
+                      ChooseImm  = 1'b0;
+                      xferByte   = 1'b0;
+                      BrTaken    = 1'b0;
+                      UncondBr   = 1'bx;
+                      ChooseMovk = 1'b0;
+                      ChooseMovz = 1'b0;
+                      ALUOp      = 3'b010;
+                   end
+         LDURB:    begin
+                      Reg2Loc    = 1'bx;
+                      ALUSrc     = 1'b1;
+                      MemToReg   = 1'b1;
+                      RegWrite   = 1'b1;
+                      MemWrite   = 1'b0;
+                      MemRead    = 1'b1;
+                      ChooseImm  = 1'b0;
+                      xferByte   = 1'b1;
+                      BrTaken    = 1'b0;
+                      UncondBr   = 1'bx;
+                      ChooseMovk = 1'b0;
+                      ChooseMovz = 1'b0;
+                      ALUOp      = 3'b010;
+                   end
+         STUR:     begin
+                      Reg2Loc    = 1'b0;
+                      ALUSrc     = 1'b1;
+                      MemToReg   = 1'bx;
+                      RegWrite   = 1'b0;
+                      MemWrite   = 1'b1;
+                      MemRead    = 1'bx;
+                      ChooseImm  = 1'b0;
+                      xferByte   = 1'b0;
+                      BrTaken    = 1'b0;
+                      UncondBr   = 1'bx;
+                      ChooseMovk = 1'b0;
+                      ChooseMovz = 1'b0;
+                      ALUOp      = 3'b010;
+                   end
+         STURB:    begin
+                      Reg2Loc    = 1'b0;
+                      ALUSrc     = 1'b1;
+                      MemToReg   = 1'bx;
+                      RegWrite   = 1'b0;
+                      MemWrite   = 1'b1;
+                      MemRead    = 1'bx;
+                      ChooseImm  = 1'b0;
+                      xferByte   = 1'b0;
+                      BrTaken    = 1'b0;
+                      UncondBr   = 1'bx;
+                      ChooseMovk = 1'b0;
+                      ChooseMovz = 1'b0;
+                      ALUOp      = 3'b010;
+                   end
+         MOVK:     begin
+                      Reg2Loc    = 1'bx;
+                      ALUSrc     = 1'bx;
+                      MemToReg   = 1'b0;
+                      RegWrite   = 1'b1;
+                      MemWrite   = 1'b0;
+                      MemRead    = 1'bx;
+                      ChooseImm  = 1'bx;
+                      xferByte   = 1'bx;
+                      BrTaken    = 1'b0;
+                      UncondBr   = 1'bx;
+                      ChooseMovk = 1'b1;
+                      ChooseMovz = 1'b0;
+                      ALUOp      = 3'b000;
+                   end
+         MOVZ:     begin
+                      Reg2Loc    = 1'bx;
+                      ALUSrc     = 1'bx;
+                      MemToReg   = 1'b0;
+                      RegWrite   = 1'b1;
+                      MemWrite   = 1'b0;
+                      MemRead    = 1'bx;
+                      ChooseImm  = 1'bx;
+                      xferByte   = 1'bx;
+                      BrTaken    = 1'b0;
+                      UncondBr   = 1'bx;
+                      ChooseMovk = 1'bx;
+                      ChooseMovz = 1'b1;
+                      ALUOp      = 3'b000;
+                   end
+         default:  begin
+                      Reg2Loc    = 1'bx;
+                      ALUSrc     = 1'bx;
+                      MemToReg   = 1'bx;
+                      RegWrite   = 1'b0;
+                      MemWrite   = 1'b0;
+                      MemRead    = 1'bx;
+                      ChooseImm  = 1'bx;
+                      xferByte   = 1'bx;
+                      BrTaken    = 1'b0;
+                      UncondBr   = 1'bx;
+                      ChooseMovk = 1'bx;
+                      ChooseMovz = 1'bx;
+                      ALUOp      = 3'b000;
+                   end
+   endcase
 endmodule
